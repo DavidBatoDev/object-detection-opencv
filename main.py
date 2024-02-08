80404e10e54b1a707c4232859cbd765de597b981
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-02-09 00:17:19 +0800

    Configure the model

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ class_file = "coco.names"  # Path to the file that contains the class names
 with open(class_file, "rt") as f:
     class_name = f.read().rstrip("\n").split("\n")  # Read the class names
 
-print(class_name)  # Print the class names
+config_path = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt" # Path to the configuration file
+
+weights_path = "frozen_inference_graph.pb"  # Path to the pre-trained model
+
+net = cv2.dnn_DetectionModel(weights_path, config_path)  # Load the model
+
+net.setInputSize(320, 320)  # Set the input size
+
+net.setInputScale(1.0 / 127.5)  # Set the input scale
+
+net.setInputMean((127.5, 127.5, 127.5))  # Set the input mean
+
+net.setInputSwapRB(True)  # Set the input swapRB
 
 cv2.imshow("Output", img)  # Display the image
 
