01a98ec5ea996867ec0e32358a7d7a4898074fd0
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-02-09 00:31:41 +0800

    Setting up webcam

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import cv2
 
-img = cv2.imread("lena.png")  # Load an image
+#img = cv2.imread("lena.png")  # Load an image
+cap = cv2.VideoCapture(0)  # Load a video
+
+cap.set(3, 640)  # Set the width
+cap.set(4, 480)  # Set the height
+cap.set(10, 100)  # Set the brightness
+
+while True:  # Loop through the video
+    success, img = cap.read()  # Read the video
+    cv2.imshow("Video", img)  # Display the video
+    if cv2.waitKey(1) & 0xFF == ord("q"):  # Wait for a key press
+        break  # Break the loop
+
+# Load the pre-trained model
 
 class_name = []  # Create an empty list to store the class names
 class_file = "coco.names"  # Path to the file that contains the class names
@@ -18,13 +31,15 @@ net.setInputScale(1.0 / 127.5)  # Set the input scale
 net.setInputMean((127.5, 127.5, 127.5))  # Set the input mean
 net.setInputSwapRB(True)  # Set the input swapRB
 
-class_ids, confs, bbox = net.detect(img, confThreshold=0.5)  # Detect objects in the image
+while True:  # Loop through the video
+    success, img = cap.read()  # Read the video
+    class_ids, confs, bbox = net.detect(img, confThreshold=0.5)  # Detect objects in the image
 
-for class_id, confidence, box in zip(class_ids.flatten(), confs.flatten(), bbox):  # Loop through the detected objects
-    cv2.rectangle(img, box, (0, 255, 0), 2)  # Draw a rectangle around the object
-    cv2.putText(img, class_name[class_id - 1].upper(), (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)  # Put the class name on the image
-    cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)  # Put the confidence on the
-    
+    if len(class_ids) != 0:  # If there are objects in the image
+        for class_id, confidence, box in zip(class_ids.flatten(), confs.flatten(), bbox):  # Loop through the detected objects
+            cv2.rectangle(img, box, (0, 255, 0), 2)  # Draw a rectangle around the object
+            cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)  # Put the confidence on the
+            
 
-cv2.imshow("Output", img)  # Display the image
-cv2.waitKey(0)  # Wait for a key press
+    cv2.imshow("Output", img)  # Display the image
+    cv2.waitKey(0)  # Wait for a key press
