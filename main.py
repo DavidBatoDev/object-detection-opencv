93f1de9e1bbb05d9313072a33afbd0783f433d9e
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-02-09 03:58:09 +0800

    implicitly put the class names on the main file

--- main.py
+++ main.py
@@ -1,20 +1,109 @@
 import cv2
 import numpy as np
 
+color_box = (0, 255, 0)  # Create a color_box color
 thres = 0.45  # Threshold to detect objects
 nms_threshold = 0.2  # Non-maximum suppression threshold
 
-cap = cv2.VideoCapture(0)  # Load a video
+cap = cv2.VideoCapture(2)  # Load a video
 cap.set(3, 640)  # Set the width
 cap.set(4, 480)  # Set the height
 cap.set(10, 100)  # Set the brightness
 
 # Load the pre-trained model
-class_name = []  # Create an empty list to store the class names
-class_file = "coco.names"  # Path to the file that contains the class names
-
-with open(class_file, "rt") as f:
-    class_name = f.read().rstrip("\n").split("\n")  # Read the class names
+class_name = [
+    "Face",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "airplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "street sign",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "hat",
+    "backpack",
+    "umbrella",
+    "shoe",
+    "eye glasses",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "plate",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "couch",
+    "potted plant",
+    "bed",
+    "mirror",
+    "dining table",
+    "window",
+    "desk",
+    "toilet",
+    "door",
+    "tv",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "blender",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+    "hair brush",
+]  # Create an empty list to store the class names
 
 config_path = (
     "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"  # Path to the configuration file
@@ -48,7 +137,7 @@ while True:  # Loop through the video
         box = bbox[i]
         x, y, w, h = box[0], box[1], box[2], box[3]
         cv2.rectangle(
-            img, (x, y), (x + w, h + y), color=(0, 255, 0), thickness=2
+            img, (x, y), (x + w, h + y), color=color_box, thickness=2
         )  # Draw a rectangle around the object
         cv2.putText(
             img,
@@ -56,7 +145,7 @@ while True:  # Loop through the video
             (box[0] + 10, box[1] + 50),
             cv2.FONT_HERSHEY_COMPLEX,
             2,
-            (0, 255, 0),
+            color_box,
             2,
         )  # Put the class name on the image
 
